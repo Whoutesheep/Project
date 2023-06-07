6e60ebe4157da80b2ad524b503d320689632316f
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-07 14:17:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 echo "Installation of ActivityWatch"
 git clone --recursive https://github.com/ActivityWatch/activitywatch.git
 echo "Checking dependencies"
+python3 -V
 if ! type python3 -V >/dev/null 2>&1; then
     echo "Python3 not installed" 1>&2
 else 
