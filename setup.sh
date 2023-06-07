e54a61c68a350f0e5b7640d570904e996b577c47
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-07 16:46:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Installation of ActivityWatch"
-#git clone --recursive https://github.com/ActivityWatch/activitywatch.git
+git clone --recursive https://github.com/ActivityWatch/activitywatch.git
 echo "Checking dependencies"
 python3 -V
 export PATH="$HOME/.local/bin:$PATH"
@@ -8,8 +8,8 @@ $PATH
 echo "EXPORT PATH"
 curl -sSL https://install.python-poetry.org |python3 - #download poetry
 poetry -V
-#sudo apt install nodejs
-#node -v
-#sudo apt install npm
-#npm -v
-#sudo apt-get install python3-venv
+sudo apt install nodejs
+node -v
+sudo apt install npm
+npm -v
+sudo apt-get install python3-venv
