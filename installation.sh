77f19f210904546f40970f09637dbb53e3bfeadc
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-08 15:00:10 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#use source to run the program, otherwise the export won't stay in the terminal (check for bashrc file to make it permanent) 
 echo "Installation of ActivityWatch"
 git clone --recursive https://github.com/ActivityWatch/activitywatch.git
 echo "Checking dependencies"
