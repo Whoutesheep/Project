40dce419dc1591d593980a68553b36aa097c1851
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-09 11:32:42 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-echo -e "------------------\Installation of ActivityWatch\------------------"
-git clone --recursive https://github.com/ActivityWatch/activitywatch.git
-echo -e "------------------\Installing dependencies\------------------"
+echo -e "------------------------------------\------------------Installing dependencies------------------\------------------------------------"
 export PATH="$HOME/.local/bin:$PATH"
 curl -sSL https://install.python-poetry.org |python3 - #download poetry
 sudo apt -y install nodejs
 sudo apt -y install npm
 sudo apt-get -y install python3-pip
 python3 -m pip install --user virtualenv
+echo -e "------------------------------------\------------------Venv install------------------\------------------------------------"
+sudo python3 -m pip install --user virtualenv
+sudo apt-get -y install python3-venv
+echo -e "------------------------------------\------------------Venv launch------------------\------------------------------------"
+python3 -m venv venv_aw
+source ./venv_aw/bin/activate
+echo -e "------------------------------------\------------------Installation of ActivityWatch------------------\------------------------------------"
+git clone --recursive https://github.com/ActivityWatch/activitywatch.git
