f14e3051d2bd8587f7f3677d8bc38b07941aee18
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-09 17:15:30 +0200

    Update main_aw.sh

--- main_aw.sh
+++ main_aw.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-echo -e "------------------------------------\------------------Installing dependencies------------------/------------------------------------"
+echo "------------------------------------\------------------Installing dependencies------------------/------------------------------------"
 export PATH="$HOME/.local/bin:$PATH" #temporaire, setup_aw doit fonctionner pour etre remove
 curl -sSL https://install.python-poetry.org |python3 - 
 sudo apt -y install nodejs
 sudo apt -y install npm
 sudo apt-get -y install python3-pip
 python3 -m pip install --user virtualenv
-echo -e "------------------------------------\------------------Venv install------------------/------------------------------------"
+echo "------------------------------------\------------------Venv install------------------/------------------------------------"
 sudo python3 -m pip install --user virtualenv
 sudo apt-get -y install python3-venv
-echo -e "------------------------------------\------------------Venv launch------------------/------------------------------------"
-python3 -m venv venv_aw
+echo "------------------------------------\------------------Venv launch------------------/------------------------------------"
+sudo python3 -m venv venv_aw
 source ./venv_aw/bin/activate
-echo -e "------------------------------------\------------------Installation of ActivityWatch------------------/------------------------------------"
-source git clone --recursive https://github.com/ActivityWatch/activitywatch.git
+echo "------------------------------------\------------------Installation of ActivityWatch------------------/------------------------------------"
+sudo git clone --recursive https://github.com/ActivityWatch/activitywatch.git
