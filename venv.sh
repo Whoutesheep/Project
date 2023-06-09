c3c4c7ee24d07b60faf1fd09a7aa85905a46757a
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-09 11:38:57 +0200

    Delete venv.sh

--- venv.sh
+++
@@ -1,5 +0,0 @@
-echo -e "------------------\Venv install\------------------"
-sudo python3 -m pip install --user virtualenv
-sudo apt-get -y install python3-venv
-python3 -m venv venv_aw
-source ./venv_aw/bin/activate
