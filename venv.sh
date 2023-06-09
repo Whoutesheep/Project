34a633af46b56881349b7e495152d7cff086804e
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-09 10:45:10 +0200

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -1,4 +1,4 @@
-echo "Venv install"
+echo -e "------------------\Venv install\------------------"
 sudo python3 -m pip install --user virtualenv
 sudo apt-get -y install python3-venv
 python3 -m venv venv_aw
