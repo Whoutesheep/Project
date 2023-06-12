d95cd981df292332312f3ed774d57fe4142eeff4
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-12 15:59:46 +0200

    Update main_aw.sh

--- main_aw.sh
+++ main_aw.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 #export PATH="root/.local/bin:$PATH" #temporaire, setup_aw doit fonctionner pour etre remove
 export PATH="$HOME/.local/bin:$PATH" #temporaire, setup_aw doit fonctionner pour etre remove
-echo "------------------------------------\------------------Installation of ActivityWatch------------------/------------------------------------"
-sudo git clone --recursive https://github.com/ActivityWatch/activitywatch.git
+
 echo "------------------------------------\------------------Installing dependencies------------------/------------------------------------"
-sudo curl -sSL https://install.python-poetry.org |python3 - 
+#sudo curl -sSL https://install.python-poetry.org |python3 - 
 sudo apt -y install nodejs
 sudo apt -y install npm
 sudo apt-get -y install python3-pip
-python3 -m pip install virtualenv
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user poetry
 echo "------------------------------------\------------------Venv install------------------/------------------------------------"
-sudo python3 -m pip install --user virtualenv #--user le dl dans root --> je ne peut pas y acceder avec mes doits
+python3 -m pip install --user virtualenv
 sudo apt-get -y install python3-venv
 echo "------------------------------------\------------------Venv launch------------------/------------------------------------"
 sudo python3 -m venv venv_aw
 source ./venv_aw/bin/activate
-cd activitywatch
-sudo make build
+echo "------------------------------------\------------------Installation of ActivityWatch------------------/------------------------------------"
+sudo git clone --recursive https://github.com/ActivityWatch/activitywatch.git
+#cd activitywatch
+#sudo make build
