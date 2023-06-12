81500dc61a17de40daef962b67e9cb4c3674e220
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-12 17:01:45 +0200

    Update main_aw.sh

--- main_aw.sh
+++ main_aw.sh
@@ -2,21 +2,25 @@
 #export PATH="root/.local/bin:$PATH" #temporaire, setup_aw doit fonctionner pour etre remove
 export PATH="$HOME/.local/bin:$PATH" #temporaire, setup_aw doit fonctionner pour etre remove
 export PATH="$HOME/project/venv_aw/bin:$PATH"
-echo "------------------------------------\------------------Installing dependencies------------------/------------------------------------"
-#sudo curl -sSL https://install.python-poetry.org |python3 - 
-sudo apt -y install nodejs
-sudo apt -y install npm
 echo "------------------------------------\------------------Installing pip------------------/------------------------------------"
 sudo apt-get -y install python3-pip
 sudo python3 -m pip install --user --upgrade pip
-echo "------------------------------------\------------------Installing poetry------------------/------------------------------------"
-sudo python3 -m pip install --user poetry
 echo "------------------------------------\------------------Venv install------------------/------------------------------------"
 sudo python3 -m pip install --user virtualenv
 sudo apt-get -y install python3-venv
 echo "------------------------------------\------------------Venv launch------------------/------------------------------------"
 sudo python3 -m venv venv_aw
 source ./venv_aw/bin/activate
+
+echo "------------------------------------\------------------Installing dependencies------------------/------------------------------------"
+#sudo curl -sSL https://install.python-poetry.org |python3 - 
+sudo apt -y install nodejs
+sudo apt -y install npm
+
+echo "------------------------------------\------------------Installing poetry------------------/------------------------------------"
+sudo python3 -m pip install --user poetry
+
+
 echo "------------------------------------\------------------Installation of ActivityWatch------------------/------------------------------------"
 git clone --recursive https://github.com/ActivityWatch/activitywatch.git
 #cd activitywatch
