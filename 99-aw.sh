2aa7b77ccd2c08747706642ff6a93a3dc1f5df62
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-26 16:52:07 +0200

    Update 99-aw.sh

--- 99-aw.sh
+++ 99-aw.sh
@@ -5,7 +5,7 @@ cd ../../opt/activitywatch/aw-watcher-afk
 screen -S aw-watcher-afk
 ./aw-watcher-afk
 exit
-cd ../aw-watcher-window
-screen -S aw-watcher-window
-./aw-watcher-window
-exit
+#cd ../aw-watcher-window
+#screen -S aw-watcher-window
+#./aw-watcher-window
+#exit
