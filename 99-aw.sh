de43edca4cb384de64246d23dc169295cd7ce26a
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-26 17:10:36 +0200

    Update 99-aw.sh

--- 99-aw.sh
+++ 99-aw.sh
@@ -2,8 +2,7 @@
 #installation : etc/profile.d
 #requirements : screen / aw d'installer
 cd ../../opt/activitywatch/aw-watcher-afk
-screen -S aw-watcher-afk
-./aw-watcher-afk
+./aw-watcher-afk&
 exit
 #cd ../aw-watcher-window
 #screen -S aw-watcher-window
