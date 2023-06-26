6995ef8854728dcf365ecf4f0650c5834402526c
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-06-26 17:31:06 +0200

    Update 99-aw.sh

--- 99-aw.sh
+++ 99-aw.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 #installation : etc/profile.d
+if [ -d "/path/to/dir" ] 
+then
+  echo "ActivityWatch installed : starting watchers"
+  cd ../../opt/activitywatch/aw-watcher-afk
+  ./aw-watcher-afk&
+  cd ../aw-watcher-window
+  ./aw-watcher-window&
+else
+  echo "Error : ActivityWatch not installed"
+fi
 #Vérifier si aw est présent sur la machine
-cd ../../opt/activitywatch/aw-watcher-afk
-./aw-watcher-afk&
