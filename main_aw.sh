0478ef32cea6037b76615b8605f3487992d9edb3
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-09-05 13:30:05 +0200

    Update main_aw.sh

--- main_aw.sh
+++ main_aw.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-#export PATH="root/.local/bin:$PATH" #temporaire, setup_aw doit fonctionner pour etre remove
 export PATH="$HOME/.local/bin:$PATH" #temporaire, setup_aw doit fonctionner pour etre remove
-export PATH="$HOME/project/venv_aw/bin:$PATH"
-export PATH="opt/activitywatch/venv_aw/bin:$PATH"
-export PATH="opt/activitywatch:$PATH"
-export PATH="root/.local/bin:$PATH"
+#export PATH="$HOME/project/venv_aw/bin:$PATH"
+#export PATH="opt/activitywatch/venv_aw/bin:$PATH"
+#export PATH="opt/activitywatch:$PATH"
+#export PATH="root/.local/bin:$PATH"
 echo "------------------------------------\------------------Installing pip------------------/------------------------------------"
 sudo apt-get -y install python3-pip
 sudo python3 -m pip install --user --upgrade pip
