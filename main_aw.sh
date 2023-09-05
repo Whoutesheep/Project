b6243b750bd0148c3e5b6c3cc1fbbf1ea875ef7f
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-09-05 15:32:42 +0200

    Update main_aw.sh

--- main_aw.sh
+++ main_aw.sh
@@ -3,7 +3,7 @@ export PATH="$HOME/.local/bin:$PATH" #temporaire, setup_aw doit fonctionner pour
 cd ../activitywatch_klaim
 echo "------------------------------------\------------------Update / Upgrade------------------/------------------------------------"
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 echo "------------------------------------\------------------Installing pip------------------/------------------------------------"
 sudo apt-get -y install python3-pip
 sudo python3 -m pip install --user --upgrade pip
@@ -20,6 +20,10 @@ echo "------------------------------------\------------------Installing nodejs--
 sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 echo "------------------------------------\------------------Installing npm & python3------------------/------------------------------------"
 sudo apt -y install npm
 sudo apt -y install python3
