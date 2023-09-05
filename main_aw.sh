ddbb6f700b9d78da894abd5cb197375dd97760ad
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-09-05 14:44:54 +0200

    Update main_aw.sh

--- main_aw.sh
+++ main_aw.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 export PATH="$HOME/.local/bin:$PATH" #temporaire, setup_aw doit fonctionner pour etre remove
-#export PATH="$HOME/project/venv_aw/bin:$PATH"
-#export PATH="opt/activitywatch/venv_aw/bin:$PATH"
-#export PATH="opt/activitywatch:$PATH"
-#export PATH="root/.local/bin:$PATH"
+cd ../activitywatch_klaim
 echo "------------------------------------\------------------Installing pip------------------/------------------------------------"
 sudo apt-get -y install python3-pip
 sudo python3 -m pip install --user --upgrade pip
@@ -13,18 +10,13 @@ sudo apt-get -y install python3-venv
 echo "------------------------------------\------------------Venv launch------------------/------------------------------------"
 sudo python3 -m venv venv_aw
 source ./venv_aw/bin/activate
-
 echo "------------------------------------\------------------Installing dependencies------------------/------------------------------------"
 sudo curl -sSL https://install.python-poetry.org |python3 - 
 sudo apt -y install nodejs
 sudo apt -y install npm
 sudo apt -y install python3
-#sudo apt -y install python
-sudo apt -y install python-is-python3
-
-echo "------------------------------------\------------------Installing poetry------------------/------------------------------------"
-#sudo python3 -m pip install --user poetry
-
-
-echo "------------------------------------\------------------Installation of ActivityWatch------------------/------------------------------------"
-
+echo "------------------------------------\------------------ActivityWatch building------------------/------------------------------------"
+make clean
+make clean_all
+#make build
+#make package
