bae6d4f1b0892d98e8d25b4d142d7be393cbb22a
Whoutesheep <135707614+Whoutesheep@users.noreply.github.com>
2023-09-06 10:12:29 +0200

    Update main_aw.sh

--- main_aw.sh
+++ main_aw.sh
@@ -20,18 +20,18 @@ echo "------------------------------------\------------------Installing nodejs--
 #sudo apt -y install nodejs
 sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+sudo curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo apt-get install nodejs -y
 echo "------------------------------------\------------------Installing npm/python3/Rust------------------/------------------------------------"
 sudo apt -y install npm
-npm install -g npm@10.0.0
+sudo npm install -g npm@10.0.0
 sudo apt -y install python3
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup default nightly
-rustup update
+#sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#rustup default nightly
+#rustup update
 echo "------------------------------------\------------------ActivityWatch building------------------/------------------------------------"
 make build
 make package
